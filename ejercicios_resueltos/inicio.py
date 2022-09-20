4013445b761e7c7b4d266f7cd524b949d780bc48
Emerson Espinoza <xinayespinoza@gmail.com>
2022-09-20 01:48:37 -0300

    readme

--- ejercicios_resueltos/inicio.py
+++ ejercicios_resueltos/inicio.py
@@ -1,1 +1,10 @@
 print("hola mundo")
+
+duplicados = {"clave1": 1, "clave": 2}
+print(duplicados) 
+
+# para agregar datos a un diccionario
+
+duplicados["clav3"]= 4
+duplicados["clav1"]= 9
+print(duplicados)
